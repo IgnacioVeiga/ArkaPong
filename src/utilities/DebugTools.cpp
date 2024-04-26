4f9fe674b614f498fe8a1ea886bffdbb06c80658
IgnacioVeiga <ignacio.veiga29@gmail.com>
2024-04-26 07:12:43 -0300

    Deleted unused files

--- src/utilities/DebugTools.cpp
+++
@@ -1,6 +0,0 @@
-#include "DebugTools.h"
-
-void DebugTools::log(const std::string &message)
-{
-    std::cout << "Debug Log: " << message << std::endl;
-}
--- src/utilities/MathUtils.cpp
+++
@@ -1,6 +0,0 @@
-#include "MathUtils.h"
-
-float MathUtils::lerp(float start, float end, float t)
-{
-    return start + t * (end - start);
-}
