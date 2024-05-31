fc884c89734eeb2b2165507e605442c931a7bb0d
Ignacio Veiga <ignacio.veiga29@gmail.com>
2024-05-30 23:16:07 -0300

    New main menu

--- src/states/MenuState.cpp
+++ src/states/MenuState.cpp
@@ -19,11 +19,48 @@ MenuState::~MenuState()
 void MenuState::handleInput()
 {
     const Uint8 *keystate = SDL_GetKeyboardState(NULL);
+
+    if (keystate[SDL_SCANCODE_S])
+    {
+        selectedOption = (selectedOption + 1) % menuOptions.size();
+        SDL_Delay(250);
+        return;
+    }
+
+    if (keystate[SDL_SCANCODE_W])
+    {
+        selectedOption = (selectedOption - 1 + menuOptions.size()) % menuOptions.size();
+        SDL_Delay(250);
+        return;
+    }
+
     if (keystate[SDL_SCANCODE_SPACE])
     {
-        audioManager->playSound("game_start");
-        SDL_Delay(5000);
-        Game::flowManager->changeState(new PlayState());
+        // TODO: refactor
+        if (menuOptions[selectedOption].text == "1P")
+        {
+            audioManager->playSound("game_start");
+            SDL_Delay(5000);
+            Game::flowManager->changeState(new PlayState());
+        }
+        else if (menuOptions[selectedOption].text == "2P")
+        {
+            audioManager->playSound("game_start");
+            SDL_Delay(5000);
+            Game::flowManager->changeState(new PlayState());
+        }
+        else if (menuOptions[selectedOption].text == "Settings")
+        {
+            return;
+        }
+        else
+        {
+            Game::game_on = false;
+        }
+    }
+    if (keystate[SDL_SCANCODE_ESCAPE])
+    {
+        Game::game_on = false;
     }
 }
 
@@ -40,6 +77,25 @@ void MenuState::render()
 {
     SDL_SetRenderDrawColor(Game::renderer, 0, 0, 0, 255);
     SDL_RenderClear(Game::renderer);
-    textManager->renderText("Press SPACE to start", SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2);
+
+    textManager->renderText(GAME_TITLE, SCREEN_WIDTH / 2 - 120, 30, c_white);
+
+    // Main menu options
+    for (int i = 0; i < menuOptions.size(); ++i)
+    {
+        textManager->renderText(menuOptions[i].text, menuOptions[i].rect.x + 20, menuOptions[i].rect.y, c_white);
+
+        if (i == selectedOption)
+        {
+            SDL_SetRenderDrawColor(Game::renderer, 255, 255, 255, 127);
+        }
+        else
+        {
+            SDL_SetRenderDrawColor(Game::renderer, 0, 0, 0, 255);
+        }
+        SDL_RenderFillRect(Game::renderer, &menuOptions[i].rect);
+    }
+
+    textManager->renderText("pre-alpha 2024", SCREEN_WIDTH / 2 - 120, SCREEN_HEIGHT - 30, c_white);
     SDL_RenderPresent(Game::renderer);
 }
--- src/states/MenuState.h
+++ src/states/MenuState.h
@@ -1,16 +1,35 @@
 #ifndef MENU_STATE_H
 #define MENU_STATE_H
 
+#include "vector"
 #include "GameState.h"
 #include "TextManager.h"
 #include "AudioManager.h"
 #include "FlowManager.h"
+
+struct MenuOption
+{
+    std::string text;
+    SDL_Rect rect;
+};
 
 class MenuState : public GameState
 {
 private:
     TextManager *textManager;
     AudioManager *audioManager;
+
+    SDL_Color c_white = {255, 255, 255, 255};
+    SDL_Color c_gray = {255, 255, 255, 127};
+
+    // Opciones del menú
+    std::vector<MenuOption> menuOptions = {
+        {"1P", {350, 200, 16, 16}},
+        {"2P", {350, 250, 16, 16}},
+        {"Settings", {350, 300, 16, 16}},
+        {"Exit", {350, 350, 16, 16}}};
+
+    int selectedOption = 0;
 
 public:
     MenuState();
