d55ac7d8d63894fb707cc3b2e6e668c710e2b017
Ignacio Veiga <ignacio.veiga29@gmail.com>
2024-09-15 17:47:06 -0300

    Comments

--- src/Window.cpp
+++ src/Window.cpp
@@ -5,35 +5,44 @@
 
 bool Window::Init()
 {
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) return false;
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+        return false;
+    if (TTF_Init() == -1)
+        return false;
+    if (Mix_OpenAudio(AUDIO_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 2048) == -1)
+        return false;
 
-	if (TTF_Init() == -1) return false;
+    Uint32 windowFlags = SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE; // Resizable flag included
 
-	if (Mix_OpenAudio(AUDIO_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 2048) == -1) return false;
+    window = SDL_CreateWindow(GAME_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, windowFlags);
+    if (!window)
+        return false;
 
-	window = SDL_CreateWindow(GAME_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-	if (!window) return false;
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer)
+        return false;
 
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	if (!renderer) return false;
-    
     SetWindowMode(0, false, false);
 
-	return true;
+    return true;
 }
 
 void Window::CleanUp()
 {
-	if (renderer) SDL_DestroyRenderer(renderer);
-	if (window) SDL_DestroyWindow(window);
-	Mix_CloseAudio();
-	TTF_Quit();
-	SDL_Quit();
+    if (renderer)
+        SDL_DestroyRenderer(renderer);
+    if (window)
+        SDL_DestroyWindow(window);
+    Mix_CloseAudio();
+    TTF_Quit();
+    SDL_Quit();
 }
 
+// TODO: fix fullscreen mode in Linux, not working on KDE
 void Window::SetWindowMode(int resolutionIndex, bool fullscreen, bool borderless)
 {
-    if (resolutionIndex < 0 || resolutionIndex >= availableResolutions.size()) {
+    if (resolutionIndex < 0 || resolutionIndex >= availableResolutions.size())
+    {
         std::cerr << "Invalid resolution index" << std::endl;
         return;
     }
@@ -41,27 +50,20 @@ void Window::SetWindowMode(int resolutionIndex, bool fullscreen, bool borderless
     int width = availableResolutions[resolutionIndex].width;
     int height = availableResolutions[resolutionIndex].height;
 
-    Uint32 windowFlags = SDL_WINDOW_SHOWN;
-    if (fullscreen) {
-        windowFlags |= SDL_WINDOW_FULLSCREEN;
-    }
-    else if (borderless) {
-        windowFlags |= SDL_WINDOW_FULLSCREEN_DESKTOP;  // Borderless fullscreen
-    }
-
     SDL_SetWindowSize(window, width, height);
 
-    // Fullscreen or window
-    if (fullscreen) {
+    if (fullscreen)
+    {
         SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
     }
-    else if (borderless) {
+    else if (borderless)
+    {
         SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
     }
-    else {
-        SDL_SetWindowFullscreen(window, 0);  // Window mode
+    else
+    {
+        // Normal window mode
+        SDL_SetWindowFullscreen(window, 0); // Fullscrenn disabled
     }
-
-    // Adjust the renderer to the new size if necessary
     SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
 }
