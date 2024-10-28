e373bd0c1424cafe3586dea484f327425285aeba
Ignacio Veiga <ignacio.veiga29@gmail.com>
2024-10-27 21:07:46 -0300

    #define

--- src/Utils/Constants.h
+++ src/Utils/Constants.h
@@ -2,67 +2,59 @@
 
 #include <SDL2/SDL_pixels.h>
 
-#pragma region window
-inline const char *GAME_TITLE = "ArkaPong";
-constexpr int SCREEN_WIDTH = 512;
-constexpr int SCREEN_HEIGHT = 320;
-constexpr int AUDIO_FREQUENCY = 44100;
-#pragma endregion window
+#define GAME_TITLE "ArkaPong"
+#define SCREEN_WIDTH 512
+#define SCREEN_HEIGHT 320
+#define AUDIO_FREQUENCY 44100
 
-#pragma region entities
-constexpr int BALL_SPEED = 2;
-constexpr int BALL_SIZE = 5;
+#define BALL_SPEED 2
+#define BALL_SIZE 5
 
-constexpr int PADDLE_SPEED = 5;
-constexpr int PADDLE_OFFSET = 32;
-constexpr int PADDLE_WIDTH = 8;
-constexpr int PADDLE_HEIGHT = 32;
+#define PADDLE_SPEED 5
+#define PADDLE_OFFSET 32
+#define PADDLE_WIDTH 8
+#define PADDLE_HEIGHT 32
 
-constexpr int BRICK_WIDTH = 8;
-constexpr int BRICK_HEIGHT = 16;
-#pragma endregion entities
+#define BRICK_WIDTH 8
+#define BRICK_HEIGHT 16
 
-#pragma region colors
-inline const SDL_Color C_WHITE = {255, 255, 255, 255};
-inline const SDL_Color C_BLACK = {0, 0, 0, 255};
-inline const SDL_Color C_GRAY = {127, 127, 127, 255};
-inline const SDL_Color C_RED = {255, 0, 0, 255};
-inline const SDL_Color C_GREEN = {0, 255, 0, 255};
-inline const SDL_Color C_BLUE = {0, 0, 255, 255};
-#pragma endregion colors
+#define C_WHITE {255, 255, 255, 255}
+#define C_BLACK {0, 0, 0, 255}
+#define C_GRAY {127, 127, 127, 255}
+#define C_RED {255, 0, 0, 255}
+#define C_GREEN {0, 255, 0, 255}
+#define C_BLUE {0, 0, 255, 255}
 
-#pragma region assets
-inline const char *DOH_OP_BGM_FILEPATH = "assets/audio/bgm/doh_round.wav";
-inline const char *ENDING_BGM_FILEPATH = "assets/audio/bgm/ending.wav";
-inline const char *GAME_OVER_BGM_FILEPATH = "assets/audio/bgm/game_over.wav";
-inline const char *GAME_START_BGM_FILEPATH = "assets/audio/bgm/game_start.wav";
-inline const char *ROUND_START_BGM_FILEPATH = "assets/audio/bgm/round_start.wav";
+#define DOH_OP_BGM_FILEPATH "assets/audio/bgm/doh_round.wav"
+#define ENDING_BGM_FILEPATH "assets/audio/bgm/ending.wav"
+#define GAME_OVER_BGM_FILEPATH "assets/audio/bgm/game_over.wav"
+#define GAME_START_BGM_FILEPATH "assets/audio/bgm/game_start.wav"
+#define ROUND_START_BGM_FILEPATH "assets/audio/bgm/round_start.wav"
 
-inline const char *HIT_DOH_SFX_FILEPATH = "assets/audio/sfx/ball_hit_doh.wav";
-inline const char *HIT_ENEMY_SFX_FILEPATH = "assets/audio/sfx/ball_hit_enemy.wav";
-inline const char *HIT_OBS_SFX_FILEPATH = "assets/audio/sfx/ball_hit_obstacle.wav";
-inline const char *HIT_SHIELD_SFX_FILEPATH = "assets/audio/sfx/ball_hit_shield.wav";
-inline const char *HIT_VAUS_SFX_FILEPATH = "assets/audio/sfx/ball_hit_vaus.wav";
-inline const char *DOH_END_SFX_FILEPATH = "assets/audio/sfx/doh_disappears.wav";
-inline const char *ESCAPE_SFX_FILEPATH = "assets/audio/sfx/vaus_escape.wav";
-inline const char *EXPLOSION_SFX_FILEPATH = "assets/audio/sfx/vaus_explosion.wav";
-inline const char *HOLD_SFX_FILEPATH = "assets/audio/sfx/vaus_holds_ball.wav";
-inline const char *LIFEUP_SFX_FILEPATH = "assets/audio/sfx/vaus_life_up.wav";
-inline const char *LONG_SFX_FILEPATH = "assets/audio/sfx/vaus_longer.wav";
-inline const char *SHOT_SFX_FILEPATH = "assets/audio/sfx/vaus_shot.wav";
+#define HIT_DOH_SFX_FILEPATH "assets/audio/sfx/ball_hit_doh.wav"
+#define HIT_ENEMY_SFX_FILEPATH "assets/audio/sfx/ball_hit_enemy.wav"
+#define HIT_OBS_SFX_FILEPATH "assets/audio/sfx/ball_hit_obstacle.wav"
+#define HIT_SHIELD_SFX_FILEPATH "assets/audio/sfx/ball_hit_shield.wav"
+#define HIT_VAUS_SFX_FILEPATH "assets/audio/sfx/ball_hit_vaus.wav"
+#define DOH_END_SFX_FILEPATH "assets/audio/sfx/doh_disappears.wav"
+#define ESCAPE_SFX_FILEPATH "assets/audio/sfx/vaus_escape.wav"
+#define EXPLOSION_SFX_FILEPATH "assets/audio/sfx/vaus_explosion.wav"
+#define HOLD_SFX_FILEPATH "assets/audio/sfx/vaus_holds_ball.wav"
+#define LIFE_UP_SFX_FILEPATH "assets/audio/sfx/vaus_life_up.wav"
+#define LONG_SFX_FILEPATH "assets/audio/sfx/vaus_longer.wav"
+#define SHOT_SFX_FILEPATH "assets/audio/sfx/vaus_shot.wav"
 
-inline const char *RETRO_FONT_FILEPATH = "assets/fonts/PressStart2P-vaV7.ttf";
+#define RETRO_FONT_FILEPATH "assets/fonts/PressStart2P-vaV7.ttf"
 
-inline const char *BGS_SPRITE_FILEPATH = "assets/sprites/backgrounds.bmp";
-inline const char *BRICKS_SPRITE_FILEPATH = "assets/sprites/bricks.bmp";
-inline const char *DOH_SPRITE_FILEPATH = "assets/sprites/doh.bmp";
-inline const char *ENDING_SPRITE_FILEPATH = "assets/sprites/ending.bmp";
-inline const char *ENEMIES_SPRITE_FILEPATH = "assets/sprites/enemies.bmp";
-inline const char *POWERUPS_SPRITE_FILEPATH = "assets/sprites/powerups.bmp";
-inline const char *VAUS_SPRITE_FILEPATH = "assets/sprites/vaus.bmp";
-inline const char *WALLS_SPRITE_FILEPATH = "assets/sprites/walls.bmp";
-#pragma endregion assets
+#define BGS_SPRITE_FILEPATH "assets/sprites/backgrounds.bmp"
+#define BRICKS_SPRITE_FILEPATH "assets/sprites/bricks.bmp"
+#define DOH_SPRITE_FILEPATH "assets/sprites/doh.bmp"
+#define ENDING_SPRITE_FILEPATH "assets/sprites/ending.bmp"
+#define ENEMIES_SPRITE_FILEPATH "assets/sprites/enemies.bmp"
+#define POWER_UPS_SPRITE_FILEPATH "assets/sprites/powerups.bmp"
+#define VAUS_SPRITE_FILEPATH "assets/sprites/vaus.bmp"
+#define WALLS_SPRITE_FILEPATH "assets/sprites/walls.bmp"
 
-constexpr int CELL_GRID_SIZE = 64;
+#define CELL_GRID_SIZE 64
 
-constexpr Uint32 DEBOUNCE_TIME = 2; // In milliseconds.
+#define DEBOUNCE_TIME 5 // In milliseconds.
